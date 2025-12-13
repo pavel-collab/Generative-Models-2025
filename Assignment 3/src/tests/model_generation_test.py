6bf00bc81ed008da3672b87db3b5f0acf4ff9811
Filippenko Pavel <filippenko.ps@phystech.edu>
2025-12-13 14:57:16 +0300

    add tests to check correct model import from checkpoint and model sample generation

---
+++ Assignment 3/src/tests/model_generation_test.py
@@ -0,0 +1,49 @@
+import sys
+sys.path.append("..")
+
+from dotenv import load_dotenv
+import os
+import torch
+
+from utils import import_pretrained_generator
+from modules import DDPMConfigs
+
+load_dotenv()
+
+def test_generator_image_generation():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    try:
+        G = import_pretrained_generator(checkpoint_path=os.getenv('GENERATOR_CHECKPOINT_PATH'))
+        # Генерируем случайный шум
+        z = torch.randn(1, 100, 1, 1).to(device)
+
+        # Генерируем изображение
+        img = G(z)[0]
+
+        # Денормализация из [-1, 1] в [0, 1]
+        img = (img + 1) / 2
+        img = torch.clamp(img, 0, 1)
+    except Exception as ex:
+        print(f"Unexpected error: {ex}")
+        return False
+    else:
+        return True
+    
+def test_ddpm_image_geenration():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    try:
+        # Create configurations
+        ddpm = DDPMConfigs()
+        # Initialize
+        ddpm.init(checkpoint_path=os.getenv('DDPM_CHECKPOINT_PATH'))
+        samples = ddpm.sample()
+        samples = (samples + 1) / 2
+        samples = torch.clamp(samples, 0, 1)
+    except Exception as ex:
+        print(f"Unexpected error: {ex}")
+        return False
+    else:
+        return True
+        
+        
